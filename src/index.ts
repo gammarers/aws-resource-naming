ded4db14a30983989ef2b59aafa53a8332179a9f
yicr <yicr@users.noreply.github.com>
2024-10-31 19:34:16 +0900

    feat!: first happy code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
+import * as crypto from 'crypto';
+
+// Default
+// None (stack auto generate)
+// string
+
+// export interface Xxx {
+//   readonly naming: Naming | {functionName: string, roleName: string};
+// }
+
+export namespace ResourceNaming {
+  export enum NamingType {
+    DEFAULT,
+    NONE,
+  }
+
+  export function createRandomString(value: crypto.BinaryLike, length: number = 8) {
+    return crypto.createHash('shake256', { outputLength: (length / 2) })
+      .update(value)
+      .digest('hex');
   }
 }
