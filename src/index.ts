30e2af731277760665ba71faaf70253d516cb7e7
yicr <yicr@users.noreply.github.com>
2024-11-01 21:25:53 +0900

    feat!: add NamingType check function & Naming interface

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,12 @@ export namespace ResourceNaming {
     NONE,
   }
 
+  export interface Naming {}
+
+  export function isNamingType(value: NamingType | Naming): value is NamingType {
+    return Object.values(NamingType).includes(value as NamingType);
+  }
+
   export function createRandomString(value: crypto.BinaryLike, length: number = 8) {
     return crypto.createHash('shake256', { outputLength: (length / 2) })
       .update(value)
